c137dd93f895279f157393fff7606db9b7cd40e0
Shihotaro <etoile02@icloud.com>
2023-08-11 23:53:09 +0900

    ログイン機能の実装

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,4 +2,16 @@ class SessionController < ApplicationController
 
   def new; end
 
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+      flash[:notice] = "ログインしました"
+      session[:user_id] = user.id
+      redirect_to questions_path
+    else
+      flash.now[:alert] = "ログインに失敗しました"
+      render "new"
+    end
+  end
+
 end
