8744a7f7a13597447ef01a041c99c4a3b2320832
Shihotaro <etoile02@icloud.com>
2023-08-15 16:53:28 +0900

    体調登録機能の実装

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,12 +1,31 @@
 class RecordsController < ApplicationController
-
   def index
-    @records = Record.all
+    @records = Record.where(user_id: session[:user_id])
+    @records = @records.where(date: params[:date]) if params[:date].present?
   end
 
   def new
-    
+    @record = Record.new
   end
-  
-  
+
+  def create
+    record_params = params.require(:record).permit(
+      :date, :body_temperature, :weight, :body_fat_percentage,
+      :condition, :memo
+    )
+    record_params[:user_id] = session[:user_id]
+    @record = Record.new(record_params)
+    @record.user_id = session[:user_id]
+    if @record.save
+      flash[:notice] = '保存しました'
+      redirect_to records_path
+    else
+      flash.now[:alert] = '記録の保存に失敗しました'
+      render :new
+    end
+  end
+
+  def set_record
+    @record = Record.where(user_id: session[:user_id]).find(params[:id])
+  end
 end
