68971836a0f70a83c098866840df6dfe986e0f96
Shihotaro <etoile02@icloud.com>
2023-08-15 20:43:11 +0900

    記録詳細画面の実装

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -4,6 +4,10 @@ class RecordsController < ApplicationController
     @records = @records.where(date: params[:date]) if params[:date].present?
   end
 
+  def show
+    @record = Record.find(params[:id])
+  end
+
   def new
     @record = Record.new
   end
