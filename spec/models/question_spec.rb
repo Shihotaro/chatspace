7ec497fca8b42f94078ebb2cbf4f743f3211eead
Shihotaro <etoile02@icloud.com>
2023-08-30 13:25:02 +0900

    questionモデルのバリデーションに関するテストの追加

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  describe 'バリデーション' do
+    let(:sample_attributes) { { title: 'Sample Title', name: 'Sample Name', content: 'Sample Content' } }
+    let(:question) { Question.new(sample_attributes) }
+
+    it '有効な値でバリデーションが通ること' do
+      expect(question).to be_valid
+    end
+
+    it 'タイトルがnilの場合、バリデーションが通らないこと' do
+      question.title = nil
+      expect(question).to_not be_valid
+    end
+
+    it '名前がnilの場合、バリデーションが通らないこと' do
+      question.name = nil
+      expect(question).to_not be_valid
+    end
+
+    it 'コンテンツがnilの場合、バリデーションが通らないこと' do
+      question.content = nil
+      expect(question).to_not be_valid
+    end
+
+    it 'コンテンツが4文字の場合、バリデーションが通らないこと' do
+      question.content = 'mini'
+      expect(question).to_not be_valid
+    end
+
+    it 'タイトルが30文字を超えた場合、バリデーションが通らないこと' do
+      question.title = 'A' * 31
+      expect(question).to_not be_valid
+    end
+  end
+end
