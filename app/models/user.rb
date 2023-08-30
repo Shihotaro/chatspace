b29cdcc9948012350f1cd0a9694bc3cb9c2730de
Shihotaro <etoile02@icloud.com>
2023-08-30 14:33:55 +0900

    Userモデルのバリデーション修正及びテストの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   has_many :records, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 20 }
-  validates :email, presence: true, uniqueness: true, length: { maximum: 50 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,34 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'is valid with valid attributes' do
-    user = User.new(name: 'Example User', email: 'user@example.com', password: 'password')
+  let(:valid_data) { { name: 'Sample User', email: 'user@sample.com', password: 'password' } }
+  let(:user) { User.new(valid_data) }
+
+  it '有効な値でバリデーションが通ること' do
     expect(user).to be_valid
   end
 
-  it 'is not valid without a name' do
-    user = User.new(email: 'user@example.com', password: 'password')
+  it '名前がない場合、バリデーションが通らないこと' do
+    user.name = nil
     expect(user).to_not be_valid
   end
 
-  it 'is not valid without an email' do
-    user = User.new(name: 'Example User', password: 'password')
+  it 'メールアドレスがない場合、バリデーションが通らないこと' do
+    user.email = nil
     expect(user).to_not be_valid
   end
 
-  it 'is not valid with a duplicate email' do
-    User.create(name: 'Existing User', email: 'user@example.com', password: 'password')
-    user = User.new(name: 'Duplicate User', email: 'user@example.com', password: 'password')
+  it '同じメールアドレスがある場合、バリデーションが通らないこと' do
+    user.save
+    duplicate_user = User.new(name: 'Duplicate User', email: 'user@sample.com', password: 'password')
+    expect(duplicate_user).to_not be_valid
+  end
+
+  it '名前が21文字以上の場合、バリデーションが通らないこと' do
+    user.name = 'a' * 21
     expect(user).to_not be_valid
   end
 
-  it 'is not valid with too long name' do
-    user = User.new(name: 'a' * 21, email: 'user@example.com', password: 'password')
-    expect(user).to_not be_valid
-  end
-
-  it 'is not valid with too long email' do
-    user = User.new(name: 'Example User', email: "#{'a' * 51}@example.com", password: 'password')
+  it '有効ではない形式のメールアドレスの場合、バリデーションが通らないこと' do
+    user.email = 'invalid_email'
     expect(user).to_not be_valid
   end
 end
