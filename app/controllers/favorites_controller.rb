1cf87db42737cd868b22aee108e6a4fc8f55be33
Shihotaro <etoile02@icloud.com>
2023-09-16 21:51:09 +0900

    お気に入り機能の実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,13 @@
 class FavoritesController < ApplicationController
-  before_action :set_question
-
   def create
-    @favorite = Favorite.new(user_id: current_user.id, question_id: @question.id)
-    @favorite.save
+    @favorite = current_user.favorites.create(question_id: params[:question_id])
+    redirect_to questions_path
   end
 
-  def destory
-    @favorite = Favorite.find_by(user_id: current_user.id, question_id: @question.id)
-    @favorite.destory
-  end
-
-  private
-
-  def set_question
-    @question = Question.find_by(id: params[:question_id])
+  def destroy
+    @question = Question.find(params[:question_id])
+    @favorite = current_user.favorites.find_by(question_id: @question.id)
+    @favorite.destroy
+    redirect_back(fallback_location: root_path)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,13 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :records, dependent: :destroy
+  has_many :questions, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  def already_favorited?(question)
+    favorites.exists?(question_id: question.id)
+  end
 end
