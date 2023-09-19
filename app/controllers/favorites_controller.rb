444d357aeef23bf84df48db1dc523bf37ab7c698
Shihotaro <etoile02@icloud.com>
2023-09-19 17:14:33 +0900

    フラッシュメッセージの追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,20 @@
 class FavoritesController < ApplicationController
   def create
-    @favorite = current_user.favorites.create(question_id: params[:question_id])
-    redirect_back(fallback_location: root_path)
+    if current_user
+      @favorite = current_user.favorites.create(question_id: params[:question_id])
+      redirect_back(fallback_location: root_path)
+      flash[:notice] = 'お気に入りに追加しました'
+    else
+      flash[:alert] = 'お気に入り機能にはログインが必要です'
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def destroy
     @question = Question.find(params[:question_id])
     @favorite = current_user.favorites.find_by(question_id: @question.id)
     @favorite.destroy
+    flash[:notice] = 'お気に入りを削除しました'
     redirect_back(fallback_location: root_path)
   end
 end
