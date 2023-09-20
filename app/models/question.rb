d8ef761b9ade3cc42c959b9eacf7b34576753f20
Shihotaro <etoile02@icloud.com>
2023-09-20 21:38:42 +0900

    タグ機能実装の準備

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,8 @@ class Question < ApplicationRecord
 
   has_many :favorites, dependent: :destroy
   has_many :answers, dependent: :destroy
+  has_many :question_tags, dependent: :destroy
+  has_many :tags, through: :question_tags
 
   validates :title, presence: true, length: { maximum: 30 }
   validates :name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_914_053_844) do
+ActiveRecord::Schema[7.0].define(version: 20_230_920_113_839) do
   create_table 'answers', force: :cascade do |t|
     t.string 'name'
     t.text 'content'
@@ -28,6 +28,16 @@ ActiveRecord::Schema[7.0].define(version: 20_230_914_053_844) do
     t.index ['question_id'], name: 'index_favorites_on_question_id'
     t.index ['user_id', 'question_id'], name: 'index_favorites_on_user_id_and_question_id', unique: true
     t.index ['user_id'], name: 'index_favorites_on_user_id'
+  end
+
+  create_table 'question_tags', force: :cascade do |t|
+    t.integer 'tag_id', null: false
+    t.integer 'question_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['question_id', 'tag_id'], name: 'index_question_tags_on_question_id_and_tag_id', unique: true
+    t.index ['question_id'], name: 'index_question_tags_on_question_id'
+    t.index ['tag_id'], name: 'index_question_tags_on_tag_id'
   end
 
   create_table 'questions', force: :cascade do |t|
@@ -51,6 +61,13 @@ ActiveRecord::Schema[7.0].define(version: 20_230_914_053_844) do
     t.index ['user_id'], name: 'index_records_on_user_id'
   end
 
+  create_table 'tags', force: :cascade do |t|
+    t.string 'name', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['name'], name: 'index_tags_on_name', unique: true
+  end
+
   create_table 'users', force: :cascade do |t|
     t.string 'name'
     t.string 'email'
@@ -62,5 +79,7 @@ ActiveRecord::Schema[7.0].define(version: 20_230_914_053_844) do
   add_foreign_key 'answers', 'questions'
   add_foreign_key 'favorites', 'questions'
   add_foreign_key 'favorites', 'users'
+  add_foreign_key 'question_tags', 'questions'
+  add_foreign_key 'question_tags', 'tags'
   add_foreign_key 'records', 'users'
 end
