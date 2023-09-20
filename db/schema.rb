0b7485ff1cb5c836fa7ce2f02bf46a8bf6407766
Shihotaro <etoile02@icloud.com>
2023-09-20 23:20:41 +0900

    questionとuser_idの関連付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_920_113_839) do
+ActiveRecord::Schema[7.0].define(version: 20_230_920_140_602) do
   create_table 'answers', force: :cascade do |t|
     t.string 'name'
     t.text 'content'
@@ -46,6 +46,8 @@ ActiveRecord::Schema[7.0].define(version: 20_230_920_113_839) do
     t.text 'content'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.integer 'user_id'
+    t.index ['user_id'], name: 'index_questions_on_user_id'
   end
 
   create_table 'records', force: :cascade do |t|
