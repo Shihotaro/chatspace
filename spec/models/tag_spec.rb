4f8ee3292e2c5afd8a3dc5b09b2bc04bf0b946a3
Shihotaro <etoile02@icloud.com>
2023-09-25 21:29:31 +0900

    Tagのmodel specの追加

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    it '有効な値でバリデーションが通ること' do
+      tag = build(:tag, name: 'SampleTag')
+      expect(tag).to be_valid
+    end
+
+    it 'nameがない場合、バリデーションが通らないこと' do
+      tag = build(:tag, name: nil)
+      expect(tag).to_not be_valid
+    end
+
+    it 'nameの文字数が21以上の場合、バリデーションが通らないこと' do
+      tag = build(:tag, name: 'a' * 21)
+      expect(tag).to_not be_valid
+    end
+  end
 end
